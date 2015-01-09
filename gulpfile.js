54133471f9919bf7e84a17aee8994c9cca5fed74
monai <monai@cure.lt>
2015-01-10 00:29:15 +0200

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -50,25 +50,20 @@ gulp.task('rpm-files', [ 'rpm-setup' ], function () {
     .pipe(rpm.files());
 });
 
+gulp.task('rpm-service', [ 'rpm-setup' ], function () {
+    return gulp.src(brass.util.assets('service/systemd'))
+    .pipe(brass.util.template(options.service))
+    .pipe(brass.util.rename(options.service.name +'.service'))
+    .pipe(gulp.dest(path.join(rpm.buildRoot, '/lib/systemd/system')))
+    .pipe(rpm.files());
+});
+
 gulp.task('rpm-binaries', [ 'rpm-files' ], function () {
     return gulp.src(path.join(rpm.buildRoot, '/usr/lib/theapp/bin/theapp'))
     .pipe(brass.util.symlink([
         path.join(rpm.buildRoot, '/usr/bin/theapp'),
         path.join(rpm.buildRoot, '/usr/sbin/theapp')
     ]))
-    .pipe(rpm.files())
-    .pipe(through.obj(function (file, enc, callback) {
-        console.log(file.path);
-        console.log(rpm.options.specFileList);
-        callback(null, file);
-    }));
-});
-
-gulp.task('rpm-service', [ 'rpm-setup' ], function () {
-    return gulp.src(brass.util.assets('service/systemd'))
-    .pipe(brass.util.template(options.service))
-    .pipe(brass.util.rename(options.service.name +'.service'))
-    .pipe(gulp.dest(path.join(rpm.buildRoot, '/lib/systemd/system')))
     .pipe(rpm.files());
 });
 
@@ -78,7 +73,7 @@ gulp.task('rpm-service', [ 'rpm-setup' ], function () {
 //     .pipe(gulp.dest(rpm.buildDir_SPECS));
 // });
 
-gulp.task('rpm-spec', [ 'rpm-files' ], rpm.specTask());
+gulp.task('rpm-spec', [ 'rpm-files', 'rpm-binaries' ], rpm.specTask());
  
 // gulp.task('rpm-build', [ 'rpm-setup', 'rpm-files', 'rpm-spec' ], function () {
 //     return gulp.src(path.join(rpm.buildDir_SPECS, '*'), { read: false })
