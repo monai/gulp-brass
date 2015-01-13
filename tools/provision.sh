e2134428579e6f6ffe918cace7210db90abb0a5b
monai <monai@cure.lt>
2015-01-13 22:19:17 +0200

    it works

--- tools/provision.sh
+++ tools/provision.sh
@@ -3,21 +3,17 @@
 centos64() {
     install_epel http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
     
-    yum -y -q --nogpgcheck update
-    yum -y -q --nogpgcheck groupinstall "Development Tools"
     yum -y -q --nogpgcheck install kernel-devel
     
-    install_guest_additions
+    /etc/init.d/vboxadd start
 }
 
 centos70() {
     install_epel http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
     
-    # yum -y -q --nogpgcheck groups mark install "Development Tools"
-    # yum -y -q --nogpgcheck update
-    yum -y -q --nogpgcheck install kernel-devel
+    yum -y -q install kernel-devel
     
-    install_guest_additions
+    systemctl start vboxadd
 }
 
 centos() {
@@ -39,16 +35,6 @@ install_epel() {
     local filename=$(basename $1)
     wget -q $1
     yum -y -q --nogpgcheck install $filename
-    rm $filename
-}
-
-install_guest_additions() {
-    local version=$(curl http://download.virtualbox.org/virtualbox/LATEST.TXT)
-    local filename="VBoxGuestAdditions_${version}.iso"
-    wget -nv "http://download.virtualbox.org/virtualbox/${version}/${filename}"
-    mount -o loop $filename /mnt
-    sh /mnt/VBoxLinuxAdditions.run --nox11
-    umount /mnt
     rm $filename
 }
 
