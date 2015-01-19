14fa3a71a6d15c790ed9e24578c79ea1d8771042
monai <monai@cure.lt>
2015-01-19 11:42:39 +0200

    cleanup

--- examples/theapp/gulpfile-npm.js
+++ examples/theapp/gulpfile-npm.js
@@ -2,28 +2,31 @@ var gulp = require('gulp');
 var path = require('path');
 var exec = require('child_process').exec;
 var async = require('async');
+var assign = require('lodash.assign');
 var rimraf = require('rimraf');
 var through = require('through2');
 var brass = require('../../index');
 var npm = require('gulp-brass-npm');
 
-var pkg, options;
+var pkg, options, rpm;
 
 pkg = require('./package.json');
 options = npm.getOptions(pkg);
-options.type = 'rpm';
-options.workDir = '.';
-options.target = '/usr/lib/'+ options.name;
-options.service = {
-    type: 'systemd',
-    name: options.name,
-    description: options.description,
-    target: options.target +'/bin/theapp',
-    user: 'vagrant',
-    group: 'vagrant'
-};
+options = assign(npm.getOptions(pkg), {
+    type: 'rpm',
+    workDir: '.',
+    target: '/usr/lib/'+ options.name,
+    service: {
+        type: 'systemd',
+        name: options.name,
+        description: options.description,
+        target: options.target +'/bin/theapp',
+        user: 'vagrant',
+        group: 'vagrant'
+    }
+});
 
-var rpm = brass.create(options);
+rpm = brass.create(options);
 
 gulp.task('clean', function () {
     return gulp.src([ rpm.buildDir, '*.tgz' ], { read: false })
@@ -33,11 +36,10 @@ gulp.task('clean', function () {
     }));
 });
 
-gulp.task('rpm-setup', [ 'clean' ], rpm.setupTask());
+gulp.task('setup', [ 'clean' ], rpm.setupTask());
+gulp.task('source', [ 'setup' ], npm.sourceTask(pkg, rpm));
 
-gulp.task('npm-source', [ 'rpm-setup' ], npm.sourceTask(pkg, rpm));
-
-gulp.task('rpm-files', [ 'rpm-setup', 'npm-source' ], function () {
+gulp.task('files', [ 'setup', 'source' ], function () {
     var globs = brass.util.prefix([
         '*',
         'bin/**/*',
@@ -52,16 +54,11 @@ gulp.task('rpm-files', [ 'rpm-setup', 'npm-source' ], function () {
     .pipe(rpm.files());
 });
 
-gulp.task('rpm-service', [ 'rpm-setup' ], npm.serviceTask(rpm));
+gulp.task('service', [ 'setup' ], npm.serviceTask(rpm));
+gulp.task('binaries', [ 'files' ], npm.binariesTask(pkg, rpm));
+gulp.task('spec', [ 'files', 'binaries' ], rpm.specTask());
+gulp.task('build', [ 'setup', 'source', 'files', 'binaries', 'service', 'spec' ], rpm.buildTask());
 
-gulp.task('rpm-binaries', [ 'rpm-files' ], npm.binariesTask(pkg, rpm));
-
-gulp.task('rpm-spec', [ 'rpm-files', 'rpm-binaries' ], rpm.specTask());
- 
-gulp.task('rpm-build', [ 'rpm-setup', 'npm-source', 'rpm-files', 'rpm-binaries', 'rpm-service', 'rpm-spec' ], rpm.buildTask());
-
-gulp.task('build', [ 'rpm-build' ], function () {
+gulp.task('default', [ 'build' ], function () {
     console.log('build finished');
 });
-
-gulp.task('default', [ 'build' ]);
